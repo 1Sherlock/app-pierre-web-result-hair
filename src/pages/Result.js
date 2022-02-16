de1097c0b69d16ecad8f16f1662329dcdd2baaea
nizom702 <nizom702@gmail.com>
2022-02-16 14:52:49 +0500

    morgan hair web result

--- src/pages/Result.js
+++ src/pages/Result.js
@@ -9,12 +9,36 @@ import ViewImages from "../components/ViewImages";
 
 const Result = (props) => {
     const [data, setData] = useState({});
+    const [name, setName] = useState("");
+    const [surname, setSurname] = useState("");
 
     useEffect(() => {
-        if (props.location.search?.split("batch_id=")[1]){
-            axios.get("https://v2-analysis.chowis.com:3331/web-result/cndphair/" + props.location.search?.split("batch_id=")[1])
+        if (props.location.search.includes('batch_id') && props.location.search.includes('customer_id')) {
+            let temp = props.location.search?.split("&");
+            let obj = {};
+            temp[0] = temp[0].substr(1);
+            console.log(temp)
+            obj[temp[0].split('=')[0]] = temp[0].split('=')[1];
+            obj[temp[1].split('=')[0]] = temp[1].split('=')[1];
+            console.log(obj);
+            axios.post("https://v2-app.chowis.com/api/customers/generate_token")
+                .then(res => {
+                    console.log(res.data.token);
+                    axios.get("https://v2-app.chowis.com/api/customers/" + obj.customer_id, {
+                        headers: {
+                            'X-CHOWIS-TOKEN': res.data.token
+                        }
+                    })
+                        .then(response => {
+                            console.log(response)
+                            setName(response.data.name);
+                            setSurname(response.data.surname);
+                        })
+                })
+            axios.get("https://v2-analysis.chowis.com:3331/web-result/cndphair/" + obj.batch_id)
+
                 .then((res) => {
-                    if (res.data.body){
+                    if (res.data.body) {
                         setData({
                             density: res.data.body.filter(item => item.measurement === "density")[0],
                             hairloss: res.data.body.filter(item => item.measurement === "hairloss")[0],
@@ -28,19 +52,17 @@ const Result = (props) => {
         }
     }, [])
 
-    console.log(data);
-
     const getBackgroundColor = (item, value) => {
-        if (item === 'density' || item === 'hairloss' || item === 'scalpKeratin' || item === 'scalpRedness' || item === "thickness"){
-            if (value >=0 && value <= 20){
+        if (item === 'density' || item === 'hairloss' || item === 'scalpKeratin' || item === 'scalpRedness' || item === "thickness") {
+            if (value >= 0 && value <= 20) {
                 return 'excellent'
-            } else if (value > 20 && value <= 30){
+            } else if (value > 20 && value <= 30) {
                 return 'good'
-            } else if (value > 30 && value <= 50){
+            } else if (value > 30 && value <= 50) {
                 return 'moderate'
-            } else if (value > 50 && value <= 80){
+            } else if (value > 50 && value <= 80) {
                 return 'poor'
-            } else if (value > 80){
+            } else if (value > 80) {
                 return 'very-poor'
             }
         }
@@ -50,14 +72,14 @@ const Result = (props) => {
         <div>
             <div className="header">
                 <div className="header-logo">
-                    <img src="/images/logo.png" alt=""/>
+                    <img src="/images/logo.svg" alt=""/>
                 </div>
             </div>
 
             <div className="analysis-result">
                 <div className="">
                     <div className="name-person">
-                        <h5>ALEXANDer chuY - ANALYSIS Result</h5>
+                        <h5>{name && surname ? name + " " + surname + "-" : ""} ANALYSIS Result</h5>
                     </div>
                     {/*<p className="date-analysis">June 22, 2020 - 05:30:20</p>*/}
                     <p className="date-analysis">{data.density && (data.density.date.substr(0, 10) + " " + data.density.time.substr(0, 5))}</p>
@@ -65,15 +87,12 @@ const Result = (props) => {
             </div>
 
             <div className="process-part">
-                <div className="icon-average">
-                    <img src="/images/icon-average.png" alt="" className=""/>
-                    <p>Average per age group</p>
-                </div>
-
                 <div className="pores">
                     <div className="title-hyd">
                         <h5>Density</h5>
-                        <p>Result : <span className={`hyd-result ${getBackgroundColor('density', data.density?.value)}`}>{getBackgroundColor('density', data.density?.value)?.replace('-', ' ')}</span></p>
+                        <p>Result : <span
+                            className={`hyd-result ${getBackgroundColor('density', data.density?.value)}`}>{getBackgroundColor('density', data.density?.value)?.replace('-', ' ')}</span>
+                        </p>
                     </div>
 
                     <div className="process-bar">
@@ -95,7 +114,8 @@ const Result = (props) => {
                             </div>
                         </div>
                         <div className="progress">
-                            <div style={{width: data.density?.value+"%"}} className={getBackgroundColor('density', data.density?.value)}/>
+                            <div style={{width: data.density?.value + "%"}}
+                                 className={getBackgroundColor('density', data.density?.value)}/>
                         </div>
                     </div>
 
@@ -126,7 +146,9 @@ const Result = (props) => {
                 <div className="pores">
                     <div className="title-hyd">
                         <h5>Hairloss</h5>
-                        <p>Result : <span className={`hyd-result ${getBackgroundColor('hairloss', data.hairloss?.value)}`}>{getBackgroundColor('hairloss', data.hairloss?.value)?.replace('-', ' ')}</span></p>
+                        <p>Result : <span
+                            className={`hyd-result ${getBackgroundColor('hairloss', data.hairloss?.value)}`}>{getBackgroundColor('hairloss', data.hairloss?.value)?.replace('-', ' ')}</span>
+                        </p>
                     </div>
 
                     <div className="process-bar">
@@ -148,7 +170,8 @@ const Result = (props) => {
                             </div>
                         </div>
                         <div className="progress">
-                            <div style={{width: data.hairloss?.value+"%"}} className={getBackgroundColor('hairloss', data.hairloss?.value)}/>
+                            <div style={{width: data.hairloss?.value + "%"}}
+                                 className={getBackgroundColor('hairloss', data.hairloss?.value)}/>
                         </div>
                     </div>
 
@@ -179,7 +202,9 @@ const Result = (props) => {
                 <div className="pores">
                     <div className="title-hyd">
                         <h5>Scalp Keratin</h5>
-                        <p>Result : <span className={`hyd-result ${getBackgroundColor('scalpKeratin', data.scalpKeratin?.value)}`}>{getBackgroundColor('scalpKeratin', data.scalpKeratin?.value)?.replace('-', ' ')}</span></p>
+                        <p>Result : <span
+                            className={`hyd-result ${getBackgroundColor('scalpKeratin', data.scalpKeratin?.value)}`}>{getBackgroundColor('scalpKeratin', data.scalpKeratin?.value)?.replace('-', ' ')}</span>
+                        </p>
                     </div>
 
                     <div className="process-bar">
@@ -201,7 +226,8 @@ const Result = (props) => {
                             </div>
                         </div>
                         <div className="progress">
-                            <div style={{width: data.scalpKeratin?.value+"%"}} className={getBackgroundColor('scalpKeratin', data.scalpKeratin?.value)}/>
+                            <div style={{width: data.scalpKeratin?.value + "%"}}
+                                 className={getBackgroundColor('scalpKeratin', data.scalpKeratin?.value)}/>
                         </div>
                     </div>
 
@@ -232,7 +258,9 @@ const Result = (props) => {
                 <div className="pores">
                     <div className="title-hyd">
                         <h5>Scalp Redness</h5>
-                        <p>Result : <span className={`hyd-result ${getBackgroundColor('scalpRedness', data.scalpRedness?.value)}`}>{getBackgroundColor('scalpRedness', data.scalpRedness?.value)?.replace('-', ' ')}</span></p>
+                        <p>Result : <span
+                            className={`hyd-result ${getBackgroundColor('scalpRedness', data.scalpRedness?.value)}`}>{getBackgroundColor('scalpRedness', data.scalpRedness?.value)?.replace('-', ' ')}</span>
+                        </p>
                     </div>
 
                     <div className="process-bar">
@@ -254,7 +282,8 @@ const Result = (props) => {
                             </div>
                         </div>
                         <div className="progress">
-                            <div style={{width: data.scalpRedness?.value+"%"}} className={getBackgroundColor('scalpRedness', data.scalpRedness?.value)}/>
+                            <div style={{width: data.scalpRedness?.value + "%"}}
+                                 className={getBackgroundColor('scalpRedness', data.scalpRedness?.value)}/>
                         </div>
                     </div>
 
@@ -285,7 +314,9 @@ const Result = (props) => {
                 <div className="pores">
                     <div className="title-hyd">
                         <h5>Thickness</h5>
-                        <p>Result : <span className={`hyd-result ${getBackgroundColor('thickness', data.thickness?.value)}`}>{getBackgroundColor('thickness', data.thickness?.value)?.replace('-', ' ')}</span></p>
+                        <p>Result : <span
+                            className={`hyd-result ${getBackgroundColor('thickness', data.thickness?.value)}`}>{getBackgroundColor('thickness', data.thickness?.value)?.replace('-', ' ')}</span>
+                        </p>
                     </div>
 
                     <div className="process-bar">
@@ -307,7 +338,8 @@ const Result = (props) => {
                             </div>
                         </div>
                         <div className="progress">
-                            <div style={{width: data.thickness?.value+"%"}} className={getBackgroundColor('thickness', data.thickness?.value)}/>
+                            <div style={{width: data.thickness?.value + "%"}}
+                                 className={getBackgroundColor('thickness', data.thickness?.value)}/>
                         </div>
                     </div>
 
